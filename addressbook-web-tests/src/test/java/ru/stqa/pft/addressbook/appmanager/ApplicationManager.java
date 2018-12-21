2bc8d3c74563aef91fca967b2c18221cb358209d
Mikhail Cherniakov <m1234567@mail.ru>
2018-12-21 17:34:39 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +19,21 @@ public class ApplicationManager {
    private String baseUrl;
    private boolean acceptNextAlert = true;
    private StringBuffer verificationErrors = new StringBuffer();
+   private String browser;
+
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+   }
 
    public void init() {
-      driver = new FirefoxDriver();
+       if (browser == BrowserType.FIREFOX) {
+         driver = new FirefoxDriver();
+      } else if (browser == BrowserType.CHROME) {
+         driver = new ChromeDriver();
+      } else if (browser == BrowserType.EDGE) {
+         driver = new EdgeDriver();
+      }
+
       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       driver.get("http://localhost/addressbook/");
       groupHelper = new GroupHelper(driver);
