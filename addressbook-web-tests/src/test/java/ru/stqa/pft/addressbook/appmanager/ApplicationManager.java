091e5c3ead69673965b9165938c56c40f3fbbd2d
Mikhail Cherniakov <m1234567@mail.ru>
2018-12-21 12:30:27 +0300

    Выделяем специализированные классы помощники

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,14 +2,17 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
-   private WebDriver driver;
+   WebDriver driver;
+
+   private SessionHelper sessionHelper;
+   private NavigationHelper navigationHelper;
+   private GroupHelper groupHelper;
    private String baseUrl;
    private boolean acceptNextAlert = true;
    private StringBuffer verificationErrors = new StringBuffer();
@@ -18,47 +21,13 @@ public class ApplicationManager {
       driver = new FirefoxDriver();
       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       driver.get("http://localhost/addressbook/");
-      login("admin", "secret");
+      groupHelper = new GroupHelper(driver);
+      navigationHelper = new NavigationHelper(driver);
+      sessionHelper = new SessionHelper(driver);
+      sessionHelper.login("admin", "secret");
    }
 
-   public void login(String username, String password) {
-      driver.findElement(By.name("user")).clear();
-      driver.findElement(By.name("user")).sendKeys(username);
-      driver.findElement(By.name("pass")).click();
-      driver.findElement(By.name("pass")).clear();
-      driver.findElement(By.name("pass")).sendKeys(password);
-      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-   }
-
-   public void returnToGroupPage() {
-      driver.findElement(By.linkText("group page")).click();
-   }
-
-   public void submitGroupeCreation() {
-      driver.findElement(By.name("submit")).click();
-   }
-
-   public void fillGroupForm(GroupData groupData) {
-      driver.findElement(By.name("group_name")).click();
-      driver.findElement(By.name("group_name")).clear();
-      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-      driver.findElement(By.name("group_header")).click();
-      driver.findElement(By.name("group_header")).clear();
-      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-      driver.findElement(By.name("group_footer")).click();
-      driver.findElement(By.name("group_footer")).clear();
-      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-   }
-
-   public void initGroupCreation() {
-      driver.findElement(By.name("new")).click();
-   }
-
-   public void gotoGroupPage() {
-      driver.findElement(By.linkText("groups")).click();
-   }
-
-   public void stop() {
+    public void stop() {
       driver.quit();
       String verificationErrorString = verificationErrors.toString();
       if (!"".equals(verificationErrorString)) {
@@ -99,11 +68,11 @@ public class ApplicationManager {
       }
    }
 
-   public void deleteSelectedGroups() {
-      driver.findElement(By.name("delete")).click();
+   public GroupHelper getGroupHelper() {
+      return groupHelper;
    }
 
-   public void selectGroup() {
-      driver.findElement(By.name("selected[]")).click();
+   public NavigationHelper getNavigationHelper() {
+      return navigationHelper;
    }
 }
